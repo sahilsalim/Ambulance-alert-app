9e06b831ce6ff921b378bf0e01571f4d236c6439
dixitsanskar <sanskar.sanjay@gmail.com>
2023-04-30 15:42:56 +0530

    Edit Map

---
+++ lib/map.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class Mapbox extends StatefulWidget {
+  const Mapbox({super.key});
+
+  @override
+  State<Mapbox> createState() => _MapboxState();
+}
+
+class _MapboxState extends State<Mapbox> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/util/config.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+final String accessToken =
+    'sk.eyJ1IjoiY3lieTEwMDIiLCJhIjoiY2xoMjRxenRsMWFrcTNkcnphZW1oaHpuYiJ9.1s7I24EFzusU3R1r1CoMpw';
+    
